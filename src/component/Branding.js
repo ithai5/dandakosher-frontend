9c0dad5e12dac07537a07d3e4d864078ff58df5a
Itai Gramse <ithai.g@gmail.com>
2020-12-04 13:00:14 +0100

    add logo

--- src/component/Branding.js
+++ src/component/Branding.js
@@ -4,7 +4,7 @@ class Branding extends Component {
     render() {
         return (
             <div id="branding">
-                <img src="https://lh3.googleusercontent.com/2uTmf4saMHym6dlzYt0DsUwti-imkV7VtCR_tCFPhuWQ8mFnSffGVtMBm0uJHJb9JbFCNq9un4MouyRuXfLczFi3FDrd6St7mVNNWHNydzqaIZ3ElnlJbiMuNnsVjP1u76zj-DTwzAPVrwHfEZnjo6-mSqYS4FB2ufSZ_FoKwoTsKR-KRcSXjlpJaGU55diFFuhq-OjijUL4qyZwgte7bzTvCKC-GsXbfQwk10Vz3Nmg84wdDMQo-1WsEzI5p5VOcBjBHNVxUw8N_3Vkb1Ci2_HNuxzm35kZ2kGhx7HaD1_4PVCRUcGpxW-Pbi18F-_flyOXG7CjTbdxs3V40P_nSEv5cKEeoc_4tTUg7-143rDwI0SP-m0EiAk2XTBhtWUr9HmcQzjybCX-d-6kk0m4YQpOq3u2Z6XOv8L4Lz8i4NILZbvwz76JsAhP6VY-I9QxMjYYksCanwlRdzyYUFyz3aKzhbUb4LiMU9w2_ME34Jca1Fr_nkg5qwXyqxlK1vWC0_wYEKEmmLcuCcCOECxJ1DhwlINKRZ8xtVwdnhOzvh7rffC1WVO2r6REft9Dm38uqKl4QR3LVeKjYqzRqMNnGf365xQMNXGp6ie020I4jNyYlyjKj9oL93E5lrBh0MdUe3JdUXGc9qGiT-U2UGtswe8dOscM_DtM2t3PftkHsX6fh2TvULJJhXdgFr4rUrw=w424-h434-no?authuser=0"
+                <img src="logo-removebg-preview.png"
                      width="30%" height="30%"alt=""/>
             </div>
 
