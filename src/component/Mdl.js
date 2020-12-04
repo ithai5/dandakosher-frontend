397a8c84a76877d382f7fc7fb905e624c4043ee1
Ignacio <igi_07@hotmail.com>
2020-12-04 11:38:58 +0100

    Modal working

--- src/component/Mdl.js
+++ src/component/Mdl.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
-//import ReactDOM from 'react-dom';
 import Modal from 'react-responsive-modal';
 import MenuContent from "./MenuContent";
+//import 'react-responsive-modal/styles.css';
 
 export default class Mdl extends Component {
     state = {
@@ -19,9 +19,10 @@ export default class Mdl extends Component {
     render() {
         const { open } = this.state;
         return (
-            //deleted button with this.onOpenModal
-            <div onClick={this.onOpenModal}>
-                <Modal open={open} onClose={this.onCloseModal} little>
+
+            <div>
+                <button onClick={this.onOpenModal}>Press here</button>
+                <Modal open={open} onClose={this.onCloseModal} center>
                     <MenuContent menuName={this.props.menuName}/>
                 </Modal>
             </div>
