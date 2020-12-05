a1ec26f07814ba7862eed37076115902526f0691
Itai Gramse <ithai.g@gmail.com>
2020-12-05 16:20:22 +0100

    prettify WIP

--- src/component/EventsPage.js
+++ src/component/EventsPage.js
@@ -9,20 +9,22 @@ class EventsPage extends Component {
             <div>
                 <section id="boxes" >
                     <div className="container">
-                        <div className="box">
-                                <div>
-                                    <Mdl menuName={'Basic'} buttonText={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui ea corrupti nesciunt esse.'} level={1}/>
-                                </div>
-                        </div>
-                        <div className="box">
-                                <div>
-                                    <Mdl menuName={'Deluxe'} buttonText={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui ea corrupti nesciunt esse.'} level={'2'}/>
-                                </div>
-                        </div>
-                        <div className="box">
-                                <div>
-                                    <Mdl menuName={'DeluxePlus'} buttonText={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui ea corrupti nesciunt esse.'} level={'3'}/>
-                                </div>
+                        <div className='flex-container'>
+                            <div className="box">
+                                    <div>
+                                        <Mdl menuName={'Basic'} buttonText={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui ea corrupti nesciunt esse.'} level={1}/>
+                                    </div>
+                            </div>
+                            <div className="box">
+                                    <div>
+                                        <Mdl menuName={'Deluxe'} buttonText={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui ea corrupti nesciunt esse.'} level={'2'}/>
+                                    </div>
+                            </div>
+                            <div className="box">
+                                    <div>
+                                        <Mdl menuName={'DeluxePlus'} buttonText={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui ea corrupti nesciunt esse.'} level={'3'}/>
+                                    </div>
+                            </div>
                         </div>
                     </div>
                 </section>
--- src/component/QandA.js
+++ src/component/QandA.js
@@ -5,7 +5,7 @@ class QandA extends Component {
         return (
             <div className='container'>
                 <p>Q: what is the minimum order? </p>
-                <p>: Please send us a mail regarding your order and we would like to answer individually. </p>
+                <p>A: Please send us a mail regarding your order and we would like to answer individually. </p>
                 <p>Q: what is the different between the menus? </p>
                 <p>A: Each menu includes a different basic pack. For each event we would have multiple meeting were we create the menu that fits your event on best way. </p>
                 <p>Q: What do I need to order food for my event? </p>
