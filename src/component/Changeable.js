e021badb53c5bfa84db3f2f37c5fab5f2143c3bf
Itai Gramse <ithai.g@gmail.com>
2020-12-05 13:55:44 +0100

    add Q&A content

--- src/component/Changeable.js
+++ src/component/Changeable.js
@@ -3,6 +3,7 @@ import Home from "./Home";
 import EventsPage from "./EventsPage";
 import ContactUsPage from "./ContactUsPage";
 import TakeAwayPage from "./TakeAwayPage";
+import QandA from "./QandA";
 
 class Changeable extends Component {
     pageToShow(){
@@ -17,6 +18,9 @@ class Changeable extends Component {
         }
         if(this.props.currentPage === 'Take Away'){
             return <TakeAwayPage/>
+        }
+        if(this.props.currentPage ==='Q&A'){
+            return <QandA/>
         }
     }
         render() {
---
+++ src/component/QandA.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class QandA extends Component {
+    render() {
+        return (
+            <div className='container'>
+                <p>Q: what is the minimum order? </p>
+                <p>: Please send us a mail regarding your order and we would like to answer individually. </p>
+                <p>Q: what is the different between the menus? </p>
+                <p>A: Each menu includes a different basic pack. For each event we would have multiple meeting were we create the menu that fits your event on best way. </p>
+                <p>Q: What do I need to order food for my event? </p>
+                <p>A:The first thing you need to know the amount of guest you believe you would have. Next think about your budget. With those two numbers we can figure out the perfect menu for you!</p>
+            </div>
+        );
+    }
+}
+
+export default QandA;
